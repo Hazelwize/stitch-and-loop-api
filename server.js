b0e54377918c77af5910a465e0590961d92b5c7a
Micah Jackson <thehazelwize@gmail.com>
2022-06-04 23:37:13 -0400

    fixed typo

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const PORT = 8000;
 
-app.use(cors)
+
+app.use(cors())
 app.use(express.json())
+
 
 
 const stitches = [
